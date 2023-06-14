7df07e065a55cf59b083738f77b82b9520cb5242
Janay Ma <janay.ma7@gmail.com>
2023-06-13 18:46:41 -0700

    setup input to come from stdin. allow ctrl-c key to close the process

--- play.js
+++ play.js
@@ -1,26 +1,22 @@
 const net = require("net");
 const connect = require('./client.js')
 
-// // establishes a connection with the game server
-// const connect = function () {
-//   const conn = net.createConnection({
-//     host: 'localhost',
-//     port: 50541
-//   });
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+}
 
-//   // interpret incoming data as text
-//   conn.setEncoding("utf8");
-
-
-//   conn.on('data', (message) => {
-//     console.log(message);
-//   });
-
-//   return conn;
-// };
-
-
+const handleUserInput = function(key) {
+ if(key === '\u0003') {
+  process.exit();
+ }
+}
 
 console.log("Connecting ...");
+setupInput();
 
 connect();
