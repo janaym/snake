41864321070833956bb13b5d24a8975d70d07cbe
Janay Ma <janay.ma7@gmail.com>
2023-06-13 18:15:57 -0700

    add connect function to client; refactor play.js by importing connect from client

--- play.js
+++ play.js
@@ -1,22 +1,23 @@
 const net = require("net");
+const connect = require('./client.js')
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  });
+// // establishes a connection with the game server
+// const connect = function () {
+//   const conn = net.createConnection({
+//     host: 'localhost',
+//     port: 50541
+//   });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
+//   // interpret incoming data as text
+//   conn.setEncoding("utf8");
 
 
-  conn.on('data', (message) => {
-    console.log(message);
-  });
+//   conn.on('data', (message) => {
+//     console.log(message);
+//   });
 
-  return conn;
-};
+//   return conn;
+// };
 
 
 
